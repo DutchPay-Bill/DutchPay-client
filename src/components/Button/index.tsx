24abad3f9e10f73993b829eb3d19c9c71ee4e184
alyuza <alyuza.sat@gmail.com>
2024-03-01 01:05:11 +0700

    feat : add reusable button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,9 +1,16 @@
+// ButtonLogin.tsx
+import React from 'react';
 import { Button } from '@mui/material';
 import styles from './Button.module.scss';
 
-const ButtonLogin = () => {
+interface ButtonLoginProps {
+  buttonText: string;
+}
+
+const ButtonLogin: React.FC<ButtonLoginProps> = ({ buttonText }) => {
   return (
-    <Button className={styles.button} variant="contained" color="primary" fullWidth>
+    <Button className={styles.button} variant="contained" fullWidth>
+      {buttonText}
     </Button>
   );
 };
