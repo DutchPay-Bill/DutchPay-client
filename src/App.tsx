1e6a21bb9cc2022194496df7fc5c29c45b9cb9a8
alyuza <alyuza.sat@gmail.com>
2024-03-01 01:29:21 +0700

    refactor small code : add not found page and other to route path

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import PublicLayout from "./layout/PublicLayout";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import PublicLayout from './layout/PublicLayout';
+import { LoginDashBoardPage, NotFoundPage } from './pages';
 
 const App = () => {
   return (
-    <>
-      <div>App</div>;
-      <PublicLayout />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<><div>App</div><PublicLayout /></>} />
+        <Route path="/login" element={<LoginDashBoardPage />} />
+
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </Router>
   );
 };
 
