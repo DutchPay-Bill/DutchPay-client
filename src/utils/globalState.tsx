f4528d60ac64663da59fbc7d6f7d232790b2655b
RPrasetyoB <rpbasuki.dev@gmail.com>
2024-03-06 09:03:33 +0700

    feat: add cookies checker & global state management

---
+++ src/utils/globalState.tsx
@@ -0,0 +1,40 @@
+import { ReactNode, createContext, useState, Dispatch, SetStateAction } from 'react';
+
+interface Props {
+    children: ReactNode;
+}
+
+interface User{
+    username: string;
+    phone_number: string;
+    fullname: string;
+    email: string;
+}
+interface ContextProps {
+    dataChanged: boolean;
+    userData : User[];
+    setUserData : Dispatch<SetStateAction<[]>>;
+    setDataChanged: Dispatch<SetStateAction<boolean>>;
+}
+
+const defaultValue: ContextProps = {
+    userData : [],
+    setUserData : () => {},
+    dataChanged: true,
+    setDataChanged: () => {}
+};
+
+export const PublicData = createContext<ContextProps>(defaultValue);
+
+const GlobalState = ({ children }: Props) => {
+    const [userData, setUserData] = useState<[]>([])
+    const [dataChanged, setDataChanged] = useState(true)
+
+    return (
+        <PublicData.Provider value={{ userData, setUserData, dataChanged, setDataChanged }}>
+            {children}
+        </PublicData.Provider>
+    );
+};
+
+export default GlobalState;
