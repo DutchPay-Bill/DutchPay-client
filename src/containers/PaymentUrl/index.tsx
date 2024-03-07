ca39928a40df2436dca4a9551c770a894bf40c4c
rz0u <rayhan.zou98@gmail.com>
2024-03-07 22:48:28 +0700

    feat: progress on receiptPage

--- src/containers/PaymentUrl/index.tsx
+++ src/containers/PaymentUrl/index.tsx
@@ -1,10 +1,36 @@
+import { Box, IconButton, MenuItem, Select, Typography } from "@mui/material";
 import Notch from "../../components/Notch";
+import styles from "./PaymentUrl.module.scss";
+import DownloadIcon from "@mui/icons-material/Download";
 
 const PaymentUrl = () => {
   return (
     <>
       <Notch />
-      <div>PaymentUrl</div>;
+      <Box className={styles.billContainer}>
+        <Box className={styles.billHeader}>
+          <Typography>Bill Detail(kecil)</Typography>
+          <Box className={styles.billNameContainer}>
+            <Box className={styles.billName}>
+              <Typography>Bill Name(judul struk)</Typography>
+              <Typography>Bill Creator(createdby blabla at blabla)</Typography>
+            </Box>
+            <IconButton>
+              <DownloadIcon />
+            </IconButton>
+          </Box>
+          <Box className={styles.billParticipantContainer}>
+            <Typography>Hey!</Typography>
+            <Select defaultValue={"ngetest"}>
+              <MenuItem>ngetest</MenuItem>
+            </Select>
+            <Box className={styles.paymentStatus}>
+              <Typography>paid</Typography>
+            </Box>
+          </Box>
+          <Box className={styles.container}></Box>
+        </Box>
+      </Box>
     </>
   );
 };
