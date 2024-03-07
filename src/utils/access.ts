d6095488fcd9ce76c0c3481ecd154369469dff27
RPrasetyoB <rpbasuki.dev@gmail.com>
2024-03-07 22:48:22 +0700

    fix: otp bug

--- src/utils/access.ts
+++ src/utils/access.ts
@@ -1,11 +1,23 @@
 // Url
-export const API_URL = import.meta.env.VITE_API_URL
+// export const API_URL = import.meta.env.VITE_API_URL
 
 // Firebase key
-export const API_KEY = import.meta.env.VITE_API_KEY
-export const AUTH_DOMAIN = import.meta.env.VITE_AUTH_DOMAIN
-export const PROJECT_ID = import.meta.env.VITE_PROJECT_ID
-export const STORAGE_BUCKET = import.meta.env.VITE_STOREAGE_BUCKET
-export const SENDER_ID = import.meta.env.VITE_SENDER_ID
-export const APP_ID = import.meta.env.VITE_APP_ID
-export const MEASUREMENT_ID = import.meta.env.VITE_MEASUREMENT_ID
+// export const API_KEY = import.meta.env.VITE_API_KEY
+// export const AUTH_DOMAIN = import.meta.env.VITE_AUTH_DOMAIN
+// export const PROJECT_ID = import.meta.env.VITE_PROJECT_ID
+// export const STORAGE_BUCKET = import.meta.env.VITE_STOREAGE_BUCKET
+// export const SENDER_ID = import.meta.env.VITE_SENDER_ID
+// export const APP_ID = import.meta.env.VITE_APP_ID
+// export const MEASUREMENT_ID = import.meta.env.VITE_MEASUREMENT_ID
+
+// Url
+export const API_URL = "https://dutchpay-bill-server.vercel.app"
+
+// Firebase key
+export const API_KEY = "AIzaSyCjcuGcnoDgtBAIyiNhzJ7i74SNicNYiJM"
+export const AUTH_DOMAIN = "dutchpay-bill.firebaseapp.com"
+export const PROJECT_ID = "dutchpay-bill"
+export const STORAGE_BUCKET = "dutchpay-bill.appspot.com"
+export const SENDER_ID = "222654780212"
+export const APP_ID = "1:222654780212:web:80d0619ef30e3356990b61"
+export const MEASUREMENT_ID = "G-VS4DNCEJ3T"
