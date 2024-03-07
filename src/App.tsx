cab596aafe2fc7c5a38d51dee3c88065032195a1
rz0u <rayhan.zou98@gmail.com>
2024-03-07 15:16:51 +0700

    feat: notch for receipt

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,14 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { ThemeProvider } from "@emotion/react";
 import theme from "./themes/CustomTheme";
 // import PublicLayout from "./layout/PublicLayout";
-import { LoginDashBoardPage, RegisterDashboardPage, NotFoundPage } from "./pages";
+import {
+  LoginDashBoardPage,
+  RegisterDashboardPage,
+  NotFoundPage,
+} from "./pages";
 import LoadingAppPage from "./pages/LoadingAppPage";
 import GlobalState from "./utils/globalState";
+import PaymentUrlPage from "./pages/PaymentUrl";
 
 const App = () => {
   return (
@@ -15,6 +20,7 @@ const App = () => {
             <Route path="/" element={<LoadingAppPage />} />
             <Route path="/login" element={<LoginDashBoardPage />} />
             <Route path="/register" element={<RegisterDashboardPage />} />
+            <Route path="/payment" element={<PaymentUrlPage />} />
             <Route path="*" element={<NotFoundPage />} />
           </Routes>
         </Router>
---
+++ src/containers/PaymentUrl/index.tsx
@@ -0,0 +1,12 @@
+import Notch from "../../components/Notch";
+
+const PaymentUrl = () => {
+  return (
+    <>
+      <Notch />
+      <div>PaymentUrl</div>;
+    </>
+  );
+};
+
+export default PaymentUrl;
