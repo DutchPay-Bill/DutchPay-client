aba82bf48c9acafa82fa86b3cc8545ba462b1217
alyuza <alyuza.sat@gmail.com>
2024-03-13 14:19:25 +0700

    refactoring : logout button component

--- src/components/LogOut/index.tsx
+++ src/components/LogOut/index.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
-import { Box, Typography } from '@mui/material'; 
+import { Typography } from '@mui/material';
 import { Link as RouterLink } from 'react-router-dom';
 import styles from './LogOut.module.scss';
 
 const LogOut: React.FC = () => {
   return (
     <RouterLink to="/login" className={styles.root}>
-      <Box display="flex" alignItems="center">
-        <Typography variant="body1" className={styles.title}>
-          Log out
-        </Typography>
-      </Box>
+      <Typography variant="body1" className={styles.title}>
+        Log out
+      </Typography>
     </RouterLink>
   );
 };
